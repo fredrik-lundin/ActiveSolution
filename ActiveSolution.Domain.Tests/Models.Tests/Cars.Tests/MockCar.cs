08fc2bdf29f205c7f6da7fc4c960cc0a7bb94270
Fredrik Lundin <fredrik.lundin@capgemini.com>
2017-04-17 16:49:19 +0200

    test(CarBase): Add mock implementation for Car

---
+++ ActiveSolution.Domain.Tests/Models.Tests/Cars.Tests/MockCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActiveSolution.Domain.Enums;
+using ActiveSolution.Domain.Models.Cars;
+
+namespace ActiveSolution.Domain.Tests.Models.Tests.Cars.Tests
+{
+    public class MockCar: Car
+    {
+        public MockCar(string registrationNumber, CarType type, int kilometerDistance = 0) : base(registrationNumber, type, kilometerDistance)
+        {
+        }
+    }
+}
